4cb587aabce9b75548f7e8d35afad0e5d0cba386
Juan Guilherme Silva Lemos <juangsilvalemos@gmail.com>
2024-04-03 17:26:12 -0300

    Adicionar método inserirDados() para inserir novos registros na tabela funcionario
    - Adicionar método inserirDados() para inserir novos registros na tabela funcionario.
    - Usar um PreparedStatement para evitar problemas de segurança e melhorar a performance da inserção.
    - Usar SimpleDateFormat para converter a data de aniversário para o formato adequado.
    - Modificar main() para chamar inserirDados() antes de visualizarDados() para demonstrar a funcionalidade de inserção de dados.
    - Adicionar variáveis preparedStatement e sdf como atributos estáticos da classe Main para serem acessadas por outros métodos, se necessário.

--- jdbc1/src/application/Main.java
+++ jdbc1/src/application/Main.java
@@ -1,8 +1,12 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import db.DB;
 
@@ -11,18 +15,16 @@ public class Main {
 	public static Connection conn = null;
 	public static Statement statement = null;
 	public static ResultSet resultSet = null;
+	public static PreparedStatement preparedStatement = null;
+	public static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 	public static void main(String[] args) {
+		inserirDados();
 		visualizarDados();
 	}
-	
-	
-	
-	
-	
-	
 
 	public static void visualizarDados() {
+
 		try {
 			conn = DB.getConnection();
 			statement = conn.createStatement();
@@ -41,6 +43,44 @@ public class Main {
 			DB.closeStatament(statement);
 			DB.closeConnection();
 
+		}
+
+	}
+
+	public static void inserirDados() {
+
+		String insert = "INSERT INTO funcionario " + "(Nome,Email,aniversario,SalarioBase,DepartamentoId) " + "VALUES "
+				+ "(?, ?, ?, ?, ?)";
+		try {
+
+			conn = DB.getConnection();
+			preparedStatement = conn.prepareStatement(insert);
+			preparedStatement.setString(1, "Juan Guilherme");
+			preparedStatement.setString(2, "juangsilvalemos@gmail.com");
+			preparedStatement.setDate(3, new java.sql.Date(sdf.parse("27/11/2001").getTime()));
+			preparedStatement.setDouble(4, 2500.00);
+			preparedStatement.setInt(5, 4);
+
+			Integer linhasAlteradas = preparedStatement.executeUpdate();
+			System.out.println("Linhas afetadas: " + linhasAlteradas);
+
+		}
+
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		finally {
+			DB.closeStatament(preparedStatement);
+			DB.getConnection();
 		}
 
 	}
