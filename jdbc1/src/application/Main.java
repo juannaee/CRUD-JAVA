6111e8a70384c4bff84d85c409a529b193546ea8
Juan Guilherme Silva Lemos <juangsilvalemos@gmail.com>
2024-04-03 11:04:06 -0300

    Implementação de recuperação de dados do Banco de dados

--- jdbc1/src/application/Main.java
+++ jdbc1/src/application/Main.java
@@ -1,14 +1,46 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import db.DB;
 
+
+
 public class Main {
 
-	public static void main(String[] args) {
-		Connection conn = DB.getConnection();
-		DB.closeConnection();
+	public static void main(String[] args){
+		Connection conn = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
+		
+		try {
+			conn = DB.getConnection();
+			statement = conn.createStatement();
+			resultSet  = statement.executeQuery("select * from departamento");
+			
+			while(resultSet.next()) {
+				System.out.println(resultSet.getInt("Id") + ", " + resultSet.getString("Nome"));
+				
+			}
+			
+			
+			
+			
+		}catch(Exception e) {
+			e.getStackTrace();
+			
+		}
+		finally {
+			DB.closeResultSet(resultSet);
+			DB.closeStatament(statement);
+			DB.closeConnection();
+			
+		}
+		
+	
+	
 
 	}
 
