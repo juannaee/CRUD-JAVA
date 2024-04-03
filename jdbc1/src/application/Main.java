2f530e3a83eba1c35a06f7bd88c74201ad3d68b8
Juan Guilherme Silva Lemos <juangsilvalemos@gmail.com>
2024-04-03 16:51:37 -0300

    Refatorar Main para extrair lógica de visualização de dados para o método visualizarDados()
    - A lógica de visualização de dados foi movida para o método visualizarDados() para melhorar a modularidade e reutilização do código.
    - Adicionar método visualizarDados() para executar a visualização de dados.
    - Atualizar main() para chamar visualizarDados() em vez de executar diretamente a lógica de visualização de dados.
    - Fazer com que as variáveis conn, statement e resultSet sejam atributos estáticos da classe Main para serem acessadas por outros métodos, se necessário.

--- jdbc1/src/application/Main.java
+++ jdbc1/src/application/Main.java
@@ -6,41 +6,42 @@ import java.sql.Statement;
 
 import db.DB;
 
-
-
 public class Main {
 
-	public static void main(String[] args){
-		Connection conn = null;
-		Statement statement = null;
-		ResultSet resultSet = null;
-		
+	public static Connection conn = null;
+	public static Statement statement = null;
+	public static ResultSet resultSet = null;
+
+	public static void main(String[] args) {
+		visualizarDados();
+	}
+	
+	
+	
+	
+	
+	
+
+	public static void visualizarDados() {
 		try {
 			conn = DB.getConnection();
 			statement = conn.createStatement();
-			resultSet  = statement.executeQuery("select * from departamento");
-			
-			while(resultSet.next()) {
+			resultSet = statement.executeQuery("select * from departamento");
+
+			while (resultSet.next()) {
 				System.out.println(resultSet.getInt("Id") + ", " + resultSet.getString("Nome"));
-				
+
 			}
-			
-			
-			
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			e.getStackTrace();
-			
-		}
-		finally {
+
+		} finally {
 			DB.closeResultSet(resultSet);
 			DB.closeStatament(statement);
 			DB.closeConnection();
-			
+
 		}
-		
-	
-	
 
 	}
 
