f53844cf4786c99b7e8120cb260d0f5b26fe307e
Juan Guilherme Silva Lemos <juangsilvalemos@gmail.com>
2024-04-04 16:23:08 -0300

    Adicionado método para visualização dinâmica de dados no banco de dados. Agora, o método visualizarDados() aceita uma consulta SQL como parâmetro e exibe os resultados de forma dinâmica, lidando com diferentes tipos de colunas. As colunas são impressas com seus nomes e valores correspondentes, formatando datas no padrão dd/MM/yyyy quando necessário. O método menuOpcaoVisualizacao() foi adicionado para permitir que o usuário escolha entre visualizar funcionários ou departamentos. O código foi organizado e o fechamento de recursos (ResultSet, PreparedStatement, Connection) foi adicionado ao bloco finally para garantir que sejam fechados corretamente após o uso.

--- jdbc1/src/application/Main.java
+++ jdbc1/src/application/Main.java
@@ -3,46 +3,96 @@ package application;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.sql.Statement;
+//import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Scanner;
 
 import db.DB;
 
 public class Main {
-
+//	public static Statement statement = null;
+	public static Scanner sc = new Scanner(System.in);
 	public static Connection conn = null;
-	public static Statement statement = null;
 	public static ResultSet resultSet = null;
 	public static PreparedStatement preparedStatement = null;
+	public static ResultSetMetaData metaData = null;
+
 	public static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 	public static void main(String[] args) {
-		inserirDados();
-		visualizarDados();
+//		inserirDados();
+
+		visualizarDados(menuOpcaoVisualizacao());
 	}
 
-	public static void visualizarDados() {
+	public static String menuOpcaoVisualizacao() {
+
+		System.out.println("Escolha uma opção\n1 - Vizualizar Funcionarios\n2 - Vizualizar Departamentos");
+		Integer opcao = sc.nextInt();
+
+		switch (opcao) {
+		case 1: {
+			String consultaFuncionario = "select * from funcionario";
+			return consultaFuncionario;
+
+		}
+
+		case 2: {
+			String consultaDepartamento = "select * from departamento";
+			return consultaDepartamento;
+		}
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + opcao);
+		}
+
+	}
+
+	public static void visualizarDados(String consulta) {
 
 		try {
+
 			conn = DB.getConnection();
-			statement = conn.createStatement();
-			resultSet = statement.executeQuery("select * from departamento");
+			preparedStatement = conn.prepareStatement(consulta);
+			resultSet = preparedStatement.executeQuery();
+
+			metaData = resultSet.getMetaData();
+			Integer numeroDeColunas = metaData.getColumnCount();
 
 			while (resultSet.next()) {
-				System.out.println(resultSet.getInt("Id") + ", " + resultSet.getString("Nome"));
+				for (int i = 1; i <= numeroDeColunas; i++) {
+					String nomeColuna = metaData.getColumnName(i);
+					Object valorColuna = null;
 
+					if (metaData.getColumnType(i) == java.sql.Types.DATE) {
+						valorColuna = resultSet.getDate(i);
+
+					} else if (metaData.getColumnType(i) == java.sql.Types.DOUBLE) {
+						valorColuna = resultSet.getDouble(i);
+
+					} else if (metaData.getColumnType(i) == java.sql.Types.INTEGER) {
+						valorColuna = resultSet.getInt(i);
+					} else {
+						valorColuna = resultSet.getString(i);
+					}
+
+					System.out.println(nomeColuna + ": " + valorColuna);
+
+				}
+
+				System.out.println();
 			}
 
+		} catch (SQLException e) {
+			e.printStackTrace();
 		} catch (Exception e) {
-			e.getStackTrace();
-
+			e.printStackTrace();
 		} finally {
 			DB.closeResultSet(resultSet);
-			DB.closeStatament(statement);
+			DB.closeStatament(preparedStatement);
 			DB.closeConnection();
-
 		}
 
 	}
@@ -76,11 +126,6 @@ public class Main {
 
 		catch (Exception e) {
 			e.printStackTrace();
-		}
-
-		finally {
-			DB.closeStatament(preparedStatement);
-			DB.getConnection();
 		}
 
 	}
